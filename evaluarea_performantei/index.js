816bc96ddff1d67bc39051494ee10af33edc4790
Adi Cutitoiu <acutitoiu2@gmail.com>
2021-05-18 18:06:34 +0300

    add run in parallel

--- evaluarea_performantei/index.js
+++ evaluarea_performantei/index.js
@@ -1,8 +1,8 @@
 const fs = require("fs");
-const crossSpawn = require("cross-spawn");
 const path = require("path");
 const rimraf = require("rimraf");
 const CSV = require("csv-string");
+const axios = require("axios").default;
 
 const INITIAL_FOLDER = path.join(__dirname, "files", "initial");
 const COMPRESSED_FOLDER = path.join(__dirname, "files", "compressed");
@@ -21,85 +21,33 @@ const files = fs
   .readdirSync(INITIAL_FOLDER)
   .map((name) => path.join(INITIAL_FOLDER, name));
 
-function execute({ inputPath, outputPath, algorithm, decompress = false }) {
-  const arguments = [
-    "runner.js",
-    "-i",
-    inputPath,
-    "-o",
-    outputPath,
-    "-a",
-    algorithm,
-  ];
-
-  if (decompress) {
-    arguments.push("-d");
-  }
-
-  const result = crossSpawn.sync("node", arguments);
-  if (result.error) {
-    console.log(error);
-  }
-
-  const stdout = "" + result.stdout;
-  return JSON.parse(stdout);
-}
-
-function buildOutputPath({ outputPath, fileName, algorithm }) {
-  const extension = path.extname(fileName);
-  const name = path.basename(fileName, extension);
-  return path.join(outputPath, `${name}.${algorithm}${extension}`);
-}
-
-const ALGORITHMS = ["gzip", "lzma" /*"lzstring"*/];
+const SERVERS = [
+  "http://localhost:3000",
+  "http://localhost:3001",
+  "http://localhost:3002",
+];
 
 const results = [];
 
-files.forEach((filePath) => {
-  let current = {};
+async function runBenchmarks(server) {
+  while (files.length) {
+    const current = files.shift();
+    if (!current) return;
 
-  ALGORITHMS.forEach((algorithm) => {
-    const fileName = path.basename(filePath);
-    const compressedFilePath = buildOutputPath({
-      outputPath: COMPRESSED_FOLDER,
-      fileName,
-      algorithm,
-    });
-    const decompressedFilePath = buildOutputPath({
-      outputPath: DECOMPRESSED_FOLDER,
-      fileName,
-      algorithm,
-    });
+    const {data: result} = await axios.post(server, { fileId: current });
+    results.push(result);
+  }
+}
 
-    const result1 = execute({
-      inputPath: filePath,
-      outputPath: compressedFilePath,
-      algorithm,
-    });
-    const result2 = execute({
-      inputPath: compressedFilePath,
-      outputPath: decompressedFilePath,
-      algorithm,
-      decompress: true,
-    });
+async function main() {
+  await Promise.all(SERVERS.map(runBenchmarks));
 
-    current = {
-      ...current,
-      sizeKb: result1.inputFileSize,
-      [algorithm + "-compressionSizeKb"]: result1.outputFileSize,
-      [algorithm + "-compressionTimeMs"]: result1.timeMs,
-      [algorithm + "-compressionMemoryKb"]: result1.memoryKB,
-      [algorithm + "-decompressionSizeKb"]: result2.outputFileSize,
-      [algorithm + "-decompressionTimeMs"]: result2.timeMs,
-      [algorithm + "-decompressionMemoryKb"]: result2.memoryKB,
-    };
-  });
-  results.push(current);
-});
+  const csvRows = [
+    CSV.stringify(Object.keys(results[0])),
+    ...results.map((x) => CSV.stringify(x)),
+  ];
 
-const csvRows = [
-  CSV.stringify(Object.keys(results[0])),
-  ...results.map((x) => CSV.stringify(x)),
-];
+  fs.writeFileSync("./report.csv", csvRows.join(""));
+}
 
-fs.writeFileSync("./report.csv", csvRows.join(""));
+main();
---
+++ evaluarea_performantei/server-runner.js
@@ -0,0 +1,113 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const crossSpawn = require("cross-spawn");
+const path = require("path");
+
+const COMPRESSED_FOLDER = path.join(__dirname, "files", "compressed");
+const DECOMPRESSED_FOLDER = path.join(__dirname, "files", "decompressed");
+
+function execute({ inputPath, outputPath, algorithm, decompress = false }) {
+  const args = [
+    "runner.js",
+    "-i",
+    inputPath,
+    "-o",
+    outputPath,
+    "-a",
+    algorithm,
+  ];
+
+  if (decompress) {
+    args.push("-d");
+  }
+
+  const result = crossSpawn.sync("node", args);
+  if (result.error) {
+    console.log(error);
+  }
+
+  const stdout = "" + result.stdout;
+  return JSON.parse(stdout);
+}
+
+function buildOutputPath({ outputPath, fileName, algorithm }) {
+  const extension = path.extname(fileName);
+  const name = path.basename(fileName, extension);
+  return path.join(outputPath, `${name}.${algorithm}${extension}`);
+}
+
+const runForFile = ({ algorithm, filePath }) => {
+  const fileName = path.basename(filePath);
+  const compressedFilePath = buildOutputPath({
+    outputPath: COMPRESSED_FOLDER,
+    fileName,
+    algorithm,
+  });
+  const decompressedFilePath = buildOutputPath({
+    outputPath: DECOMPRESSED_FOLDER,
+    fileName,
+    algorithm,
+  });
+
+  const result1 = execute({
+    inputPath: filePath,
+    outputPath: compressedFilePath,
+    algorithm,
+  });
+  const result2 = execute({
+    inputPath: compressedFilePath,
+    outputPath: decompressedFilePath,
+    algorithm,
+    decompress: true,
+  });
+
+  return {
+    sizeKb: result1.inputFileSize,
+    [algorithm + "-compressionSizeKb"]: result1.outputFileSize,
+    [algorithm + "-compressionTimeMs"]: result1.timeMs,
+    [algorithm + "-compressionMemoryKb"]: result1.memoryKB,
+    [algorithm + "-decompressionSizeKb"]: result2.outputFileSize,
+    [algorithm + "-decompressionTimeMs"]: result2.timeMs,
+    [algorithm + "-decompressionMemoryKb"]: result2.memoryKB,
+  };
+};
+
+async function getFilePathAsync(fileId) {
+  return fileId;
+}
+
+const ALGORITHMS = ["gzip", "lzma" /*"lzstring"*/];
+
+function buildServer(port) {
+  const server = express();
+
+  server.use(bodyParser.json());
+
+  server.post("/", async (req, res, next) => {
+    try {
+      const { fileId } = req.body;
+
+      const filePath = await getFilePathAsync(fileId);
+
+      const result = ALGORITHMS.reduce(
+        (prevRes, algorithm) => ({
+          ...prevRes,
+          ...runForFile({ algorithm, filePath }),
+        }),
+        {}
+      );
+
+      res.json(result);
+    } catch (error) {
+      res.status(500).json({ error: "Something went wrong" });
+    }
+  });
+
+  server.listen(port, () => {
+    console.log(`listening on port ${port}`);
+  });
+}
+
+buildServer(3000);
+buildServer(3001);
+buildServer(3002);
