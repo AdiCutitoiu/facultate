33091329bbb69d1a22690f3c45c9b033e2f6be31
Adi Cutitoiu <acutitoiu2@gmail.com>
2021-05-31 00:18:13 +0300

    Update dimensions

--- evaluarea_performantei/file-generator.js
+++ evaluarea_performantei/file-generator.js
@@ -38,6 +38,6 @@ function generateFile(dimensionMB) {
   }
 }
 
-const DIMENSIONS = [1, 5, 10, 15, 20, 25, 30, 40, 50, 60, 70, 80, 90, 99];
+const DIMENSIONS = [1, 5, 10, 15, 20, 25, 30, 40, 50, 60, 70, 80, 90, 100];
 
 DIMENSIONS.forEach(generateFile);
